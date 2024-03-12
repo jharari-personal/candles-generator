9e682220f84bd16699ee64c6c33f8eca0f5fada4
jharari <82702084+jharari-personal@users.noreply.github.com>
2024-03-12 21:54:01 +0200

    Update candles.py

--- candles.py
+++ candles.py
@@ -11,25 +11,37 @@ def get_ATR(timeframe):
     if timeframe == "d":
         ATR1 = random.randint(80, 120)
     elif timeframe == "w":
-        ATR1 = random.randint(200, 300)
+        ATR1 = random.randint(200, 400)
     elif timeframe == "h":
-        ATR1 = random.randint(20, 30)
+        ATR1 = random.randint(20, 50)
+    elif timeframe == "m":
+        ATR1 = random.randint(5, 30)
     else:
-        ATR1 = random.randint(100, 150)
+        ATR1 = random.randint(100, 200)
     return ATR1
 
 # This function returns back the date for each candle
-def get_date(date, delta):
-    date -= timedelta(days=delta)
+def get_date(date, delta, timeframe):
+    if timeframe == "d":
+        date -= timedelta(days=delta)
+    elif timeframe == "h":
+        date -= timedelta(hours=delta)
+    elif timeframe == "m":
+        date -= timedelta(minutes=delta)
+    elif timeframe == "w":
+        date -= timedelta(weeks=delta)
     return date
         
 # Main function that generates candles
-def generate_candles(rate, timeframe, numCandles):
+def generate_candles(rate, timeframe, numCandles, date_from):
     decimals = calculate_decimal_places(rate) # In order to use ATR appropriately
     adjusted_ATR = round(get_ATR(timeframe) * 10**(-decimals), 4)
 
     candles = []
-    current_date = datetime.utcnow()
+    if date_from:
+        current_date = datetime.strptime(date_from, "%Y-%m-%dT%H:%M:%S.000Z")
+    else:
+        current_date = datetime.utcnow()
 
     for i in range(numCandles):
         candle = {}
@@ -41,6 +53,7 @@ def generate_candles(rate, timeframe, numCandles):
             candle['Low'] = round(candle['Open'] - random.uniform(0, adjusted_ATR), 4)
             candle['Close'] = round(rate, 4)
             candle['Volume'] = random.randint(1000, 100000)
+            candle['Date'] = current_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
         else:
             # For the subsequent candles, ensure the Open is equal to the last candle's Close, randomze the rest
             candle['Open'] = round(candles[-1]['Close'], 4)
@@ -48,12 +61,14 @@ def generate_candles(rate, timeframe, numCandles):
             candle['Low'] = round(candle['Open'] - random.uniform(0, adjusted_ATR), 4)
             candle['Close'] = round(random.uniform(candle['Low'], candle['High']), 4)
             candle['Volume'] = random.randint(1000, 100000)
+            candle['Date'] = ""
 
         candles.append(candle)
     
     # Now we make sure to add a date to each candle, starting from today (daily)
     for index, candle in enumerate(candles, start=1):
-        candle['Date'] = get_date(current_date, index).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        if not candle['Date']:
+            candle['Date'] = get_date(current_date, index, timeframe).strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
     candles.reverse()  # So that the last candle is equal to the seed rate 
     return candles
@@ -69,9 +84,12 @@ def export_to_csv(candles, filename='output.csv'):
 
 def main():
     rate = 1.28418
-    timeframe = "w"
-    numCandles = 1000
-
-    candles = generate_candles(rate, timeframe, numCandles)
+    timeframe = "h"
+    numCandles = 3
+   # date_from = "2023-05-17T19:24:07.000Z" # optional
+    date_from = ""
+    candles = generate_candles(rate, timeframe, numCandles, date_from)
     export_to_csv(candles)
+    print(candles)
 
+main()
